c2b48b64aa95b9476f57aee08c6d008453f7c362
James Jensen <jamesjensen1@gmail.com>
2019-10-19 18:27:16 -0600

    base theme adjustments

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
 
 Vue.config.productionTip = false;
 
